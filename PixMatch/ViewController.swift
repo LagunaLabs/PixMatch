93b98cd03a8fa115224da7ce8651435642773e5e
Justin Honda <justinohonda@gmail.com>
2019-09-19 20:21:06 -0400

    Initial Commit

---
+++ PixMatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PixMatch
+//
+//  Created by Justin Honda on 9/19/19.
+//  Copyright © 2019 Justin Honda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PixMatchTests/PixMatchTests.swift
@@ -0,0 +1,34 @@
+//
+//  PixMatchTests.swift
+//  PixMatchTests
+//
+//  Created by Justin Honda on 9/19/19.
+//  Copyright © 2019 Justin Honda. All rights reserved.
+//
+
+import XCTest
+@testable import PixMatch
+
+class PixMatchTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
